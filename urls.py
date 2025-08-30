553001aa25d9daf2b2f3ffc1214216be4a9df218
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-08-30 21:23:36 +0200

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("auth/", include("users.urls")),
-    path("properties/", include("properties.urls")),
-    path("bookings/", include("bookings.urls")),
-]
