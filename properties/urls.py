e53232ea1ac1d65e867492d23b64a77c30c6f215
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-08-30 21:24:42 +0200

    Delete properties directory

--- properties/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PropertyListCreateView, PropertyRetrieveUpdateDestroyView
-
-urlpatterns = [
-    path("", PropertyListCreateView.as_view(), name="property-list-create"),
-    path("<int:pk>/", PropertyRetrieveUpdateDestroyView.as_view(), name="property-detail"),
-]
