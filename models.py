5d545cb4af39accb24fd02bb8b45c0b986636a34
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-08-30 13:10:19 +0200

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    is_landlord = models.BooleanField(default=False)
-    # By default, is_tenant = not is_landlord (not stored, just logical)
-    
-    def __str__(self):
-        return self.username
