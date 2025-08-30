0725c1806c2507922f3dfd174c2435fcdb9a137c
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-08-30 13:14:24 +0200

    Create urls.py
    created new urls.py

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("auth/", include("users.urls")),
+    path("properties/", include("properties.urls")),
+    path("bookings/", include("bookings.urls")),
+]
