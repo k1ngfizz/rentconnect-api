4159f5d905fd332f460ae4dfe16fddd42e984bbc
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-08-31 19:07:20 +0200

    Delete bookings directory

--- bookings/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import BookingRequest
-
-class BookingRequestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BookingRequest
-        fields = '__all__'
