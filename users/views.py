3cf62e83c9f1682734627a3ebbc7c1c6821cdd08
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-08-31 19:09:43 +0200

    Delete users directory

--- users/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import RegisterSerializer
-from .models import User
-
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
